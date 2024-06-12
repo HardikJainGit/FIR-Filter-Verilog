c10cf0014fcd61cfd5abcde2fee555b412814019
Hardik Jain <133627261+HardikJainGit@users.noreply.github.com>
2024-06-12 18:26:06 +0530

    Update Bitsream_to_signal.m

--- Bitsream_to_signal.m
+++ Bitsream_to_signal.m
@@ -5,7 +5,7 @@ fclose(fid);
 
 % Create a time vector
 fs = 5; % Sample rate
-duration = length(integers) / fs * 2; 
+duration = length(integers) / fs; 
 t = linspace(0, duration, length(integers));
 
 % Plot the integers over time

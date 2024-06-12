cdca0e977c7d08589e1a1906788adfa0744a69d6
Hardik Jain <133627261+HardikJainGit@users.noreply.github.com>
2024-06-12 16:28:10 +0530

    Update FIR_Filter.v

--- FIR_Filter.v
+++ FIR_Filter.v
@@ -77,20 +77,3 @@ always@(posedge clk)
 data_out <= Add_final; 
 
 endmodule
-
-
-module DFF(clk, reset, data_in, data_delayed);
-parameter N = 16;
-input clk, reset;
-input [N-1:0] data_in;
-output reg [N-1:0] data_delayed; 
-
-always@(posedge clk or posedge reset)
-begin
-    if (reset)
-        data_delayed <= 0;
-    else
-        data_delayed <= data_in; 
-end
-
-endmodule
